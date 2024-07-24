60c8d357bcd083b510daf4a48c1f3fd143b5f7dc
ErnandesNeponuceno <ernandesmore05@gmail.com>
2024-07-24 09:48:48 -0300

    criação da sessão sobre mim

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
 const toggleTheme = document.getElementById('toggleTheme');
 const rootHtml = document.documentElement 
+const accordionHeaders = document.querySelectorAll(".accordion__header");
+const menuLinks = document.querySelectorAll(".menu__link");
+
 
 function chanceTheme (){
     const currentTheme = rootHtml.getAttribute('data-theme');
@@ -11,3 +14,19 @@ function chanceTheme (){
     toggleTheme.classList.toggle('bi-moon-stars')
 }
 toggleTheme.addEventListener('click', chanceTheme);
+
+accordionHeaders.forEach(header => {
+    header.addEventListener("click", () => {
+      const accordionItem = header.parentElement;
+      const accordionActive = accordionItem.classList.contains("active");
+  
+      accordionActive ? accordionItem.classList.remove("active") : accordionItem.classList.add("active");
+    })
+  })
+  
+  menuLinks.forEach(item => {
+    item.addEventListener("click", () => {
+      menuLinks.forEach(i => i.classList.remove("active"));
+      item.classList.add("active");
+    })
+  })
